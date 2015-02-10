f69145a8d2c806f64b2dcb55a368f15d13799e1b
Chengqian Zhao <chengqian.zh@gmail.com>
2015-02-10 21:13:34 +0100

    add all elements in the page

--- gen/com/flickr/search/R.java
+++ gen/com/flickr/search/R.java
@@ -1068,7 +1068,8 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int example=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1093,13 +1094,16 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
+        public static final int et_search=0x7f05003c;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int fragment_container=0x7f05003d;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int iv=0x7f05003f;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1109,6 +1113,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int result_list=0x7f05003e;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1183,7 +1188,9 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int fragment_research_result=0x7f030019;
+        public static final int item_result=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1226,6 +1233,9 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        /**  text for main_activity.xml 
+         */
+        public static final int activity_main_search=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
     }
--- src/com/flickr/search/MainActivity.java
+++ src/com/flickr/search/MainActivity.java
@@ -1,15 +1,99 @@
 package com.flickr.search;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	EditText et;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		et = (EditText) this.findViewById(R.id.et_search);
+
+	}
+
+	public void startSearch(View v) {
+		
+		String search_string = et.getText().toString();
+
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager
+				.beginTransaction();
+		SearchResultFragment fragment = new SearchResultFragment();
+		fragmentTransaction.add(R.id.fragment_container, fragment);
+		fragmentTransaction.commit();
+	}
+
+	class SearchResultFragment extends Fragment {
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+
+			super.onCreate(savedInstanceState);
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+
+			View v = inflater.inflate(R.layout.fragment_research_result, null);
+			ListView result_list = (ListView) v.findViewById(R.id.result_list);
+			ResultAdapter resultAdapter = new ResultAdapter();
+			result_list.setAdapter(resultAdapter);
+
+			return v;
+		}
+
+		@Override
+		public void onPause() {
+		
+			super.onPause();
+		}
+
+	}
+
+	class ResultAdapter extends BaseAdapter {
+		/**
+		 * this is the list adpater to search result
+		 */
+		@Override
+		public int getCount() {
+
+			return 5;
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+
+			return 0;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+
+			View v = MainActivity.this.getLayoutInflater().inflate(
+					R.layout.item_result, arg2, false);
+
+			return v;
+		}
+
+	}
 }
